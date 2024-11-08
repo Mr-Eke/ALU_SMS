7b6af8e6f32712f14cd886fc42fb8a8db6d240cb
Umurerwa3 <o.umurerwa@alustudent.com>
2024-11-08 09:39:54 +0200

    Display menu options for the app

---
+++ sms_app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+print("\n-----------------------------------------")
+print("\tSCHOOL MANAGEMENT SYSTEM")
+print("-----------------------------------------\n")
+
+print("1. Student Enrollment")
+print("2. Manage Student Records")
+print("3. Student Grade Input")
+print("4. CGPA Calculation")
+print("5. View Student Grades")
+print("6. Exit\n\n-----------------------------------------")
