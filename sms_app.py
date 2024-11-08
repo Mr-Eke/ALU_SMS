e231d4224637a128a84e57525b41970db3e8835e
H-levison <h.levison@alustudent.com>
2024-11-08 10:25:50 +0200

    implemented student erollment and exit menu

--- sms_app.py
+++ sms_app.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
+import sys
+from student import Student
+from enrollment import StudentEnrollment
 
-print("\n-----------------------------------------")
-print("\tSCHOOL MANAGEMENT SYSTEM")
-print("-----------------------------------------\n")
+new_student = StudentEnrollment()
 
-print("1. Student Enrollment")
-print("2. Manage Student Records")
-print("3. Student Grade Input")
-print("4. CGPA Calculation")
-print("5. View Student Grades")
-print("6. Exit\n\n-----------------------------------------")
+while True:
+    print("\n-----------------------------------------")
+    print("\tSCHOOL MANAGEMENT SYSTEM")
+    print("-----------------------------------------\n")
 
+    print("1. Student Enrollment")
+    print("2. Manage Student Records")
+    print("3. Student Grade Input")
+    print("4. CGPA Calculation")
+    print("5. View Student Grades")
+    print("6. Exit\n\n-----------------------------------------")
 
-selected_menu = int(input("Choose a menu: "))
-if selected_menu == 1:
-    # new_student.enroll_student()
+    selected_menu = int(input("Choose a menu: "))
+
+    if selected_menu == 1:
+        new_student.enroll_student()
+
+    if selected_menu == 6:
+        sys.exit()
+
