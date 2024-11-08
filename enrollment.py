b6ee483009418176d820d0bd0efa9c397c3d622d
amaliza-shal <s.amaliza@alustudent.com>
2024-11-08 09:46:28 +0200

    defined a class to manage student erollment

---
+++ enrollment.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from student import Student
+
+
+class StudentEnrollment:
+
+    def __init__(self):
+        self.students = {}
+
+    def enroll_student(self):
+        # try:
+        full_name = input("\nEnter first and last name seperated with space: ")
+        fs_name, ls_name = full_name.split(" ")
+    # except ValueError:
+        intake = input("Enter your intake (e.g 2024M for May 2024 intake): ")
+        trimester = input("Enter your trimester (e.g T2 for trimester 2): ")
