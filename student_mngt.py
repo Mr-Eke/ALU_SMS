e24e8e8f579ac283912fac184d1ce2e17fd7129d
Az-oga <a.oga@alustudent.com>
2024-11-08 13:51:38 +0200

    Implemented a method for viewing a spaciific student by ID

--- student_mngt.py
+++ student_mngt.py
@@ -7,5 +7,16 @@ class ManageStudentsRecord:
         self.enrolled_student =  enrolled_student
     def view_students(self):
 
+        print("\n===============================================================")
+        print("\tList of Enrolled Students\n-----------------------------------------")
         for student_attr in self.enrolled_student.students.values():
             print(student_attr)
+        print("===============================================================")
+
+    def view_student_by_id(self):
+        id_input = int(input("Enter student ID: "))
+        for student_id, val in self.enrolled_student.students.items():
+            if student_id == id_input:
+                print(val)
+            else:
+                print("Theres no Student with such ID")
