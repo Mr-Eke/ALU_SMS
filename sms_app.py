e20a9843f1105d7a5107e544e907b0d0640daf9f
Umurerwa3 <o.umurerwa@alustudent.com>
2024-11-22 22:32:56 +0200

    Refactoring: changed all menu list to fit new implementation

--- sms_app.py
+++ sms_app.py
@@ -5,7 +5,7 @@ from models.assignment import Assignment
 from models.student_mngt import SchoolManagementSystem
 
 def main():
-    system = SchoolManagementSystem()
+    system_manager = SchoolManagementSystem()
     while True:
         print("\n\tSCHOOL MANAGEMENT SYSTEM")
         print("-----------------------------------------\n")
@@ -15,14 +15,14 @@ def main():
         print("5. Exit\n")
         choice = input("Choose an option: ")
         if choice == "1":
-            system.enroll_student()
+            system_manager.enroll_student()
         elif choice == "2":
-            system.add_assignment()
+            system_manager.add_assignment()
         elif choice == "3":
             student_id = int(input("Enter student ID: "))
-            system.calculate_overall_score(student_id)
+            system_manager.calculate_overall_score(student_id)
         elif choice == "4":
-            system.view_student_records()
+            system_manager.view_student_records()
         elif choice == "5":
             exit_option = input("are you sure you want to exit (yes/no): ").lower()
             sys.exit() if exit_option == "yes" else ""
