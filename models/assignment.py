0e6d975c50419957ac91f2771a8585b25eb8cd09
Burabyo <j.burabyo@alustudent.com>
2024-11-23 18:18:33 +0200

    Documented the assignment class and its method

--- models/assignment.py
+++ models/assignment.py
@@ -17,13 +17,29 @@ def save_data(data, file_name):
         json.dump(data, file, indent=4)
 
 class Assignment:
-    """Defines an assignment
     """
+    Represents an assignment with its details.
+    Attributes:
+        name (str): The name of the assignment.
+        assig_type (str): assignment (e.g., "Formative" or "Summative").
+        score (float): The score of the assignment (out of 100).
+        weight (float): assignment weight (percentage of total course weight).
+    """
+
     def __init__(self, name, assig_type, score, weight):
+        """
+        Initializes an Assignment instance.
+        """
         self.name = name
         self.assig_type = assig_type
         self.score = score
         self.weight = weight
 
     def get_weighted_score(self):
+        """ Calculates and return sthe weighted score of the assignment.
+
+        Derived by multiplying the score (as a percentage)
+        by the weight of the assignment. """
+
         return (self.score / 100) * self.weight
+
